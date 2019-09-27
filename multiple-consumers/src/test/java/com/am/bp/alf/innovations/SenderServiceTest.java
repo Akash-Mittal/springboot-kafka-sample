eeb07475976f32c3d391818cbff79682bca7d015
Akash-Mittal <mittal.akash.adam@gmail.com>
2019-09-27 18:32:41 +0200

    Changes:1.) https://github.com/Akash-Mittal/springboot-kafka-sample/issues/2

--- multiple-consumers/src/test/java/com/am/bp/alf/innovations/SenderServiceTest.java
+++ multiple-consumers/src/test/java/com/am/bp/alf/innovations/SenderServiceTest.java
@@ -7,8 +7,10 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
@@ -19,12 +21,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { ListenerConfig.class, KafkaListenerEndpointRegistry.class,
         ListenerService.class }, webEnvironment = WebEnvironment.NONE)
-@ActiveProfiles("test-junit")
+@ActiveProfiles("junittest")
+@ComponentScan("com.am.bp.alf.innovations")
+@EntityScan("com.am.bp.alf.innovations")
 public class SenderServiceTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SenderServiceTest.class);
 
-    private static String BATCH_TOPIC = "batch.t";
+    private static String BATCH_TOPIC = "junittest.user.activity.logs.t";
 
     @Autowired
     private SenderService senderService;
